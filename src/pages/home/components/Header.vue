af235ec8a3b55ed015187695e93c8f3d1169d2b3
shaw-555 <shaw18664590570@163.com>
2021-04-25 20:23:58 +0800

    add header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
         <div class="header">
-          <div class="header-left">left</div>
-          <div class="header-input">输入城市/景点/游玩主题</div>
-          <div class="header-right">right</div>
+          <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+          </div>
+          <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题</div>
+          <div class="header-right">
+            城市
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+          </div>
         </div>
     </div>
 </template>
@@ -15,15 +22,19 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
   .header
     display: flex
     height: .86rem
     line-height: .86rem
-    background: #00bcd4
+    background: $bgColor
     color: #fff
     .header-left
       width: .64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-input
       flex: 1
       background: #fff
@@ -31,10 +42,14 @@ export default {
       margin-top: .12rem
       height: .64rem
       margin-left: .2rem
+      padding-left: .2rem
       color: #ccc
       line-height: .64rem
     .header-right
       with: 1.24rem
       float: right
       text-align: center
+      padding-left: .1rem
+      .arrow-icon
+       font-size: .24rem
 </style>
