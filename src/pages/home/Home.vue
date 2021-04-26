b8260c32875babdce6d8403bf03ce85de3bc2ee8
shaw-555 <shaw18664590570@163.com>
2021-04-26 22:40:54 +0800

    推荐组件

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
        <home-header/>
        <home-swiper/>
        <home-icons/>
+       <home-recommend/>
        <div>test</div>
     </div>
 </template>
@@ -11,13 +12,15 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   }
 }
 </script>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul v-for="item in recommendList" :key="item.id">
+      <li class="item border-bottom">
+          <img :src="item.imgUrl" class="item-img">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="itme-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          title: 'hehe',
+          desc: 'zzzzzz',
+          imgUrl: ''
+        },
+        {
+          id: '0002',
+          title: 'hehe',
+          desc: 'zzzzzz',
+          imgUrl: ''
+        },
+        {
+          id: '0003',
+          title: 'hehe',
+          desc: 'zzzzzz',
+          imgUrl: ''
+        },
+        {
+          id: '0004',
+          title: 'hehe',
+          desc: 'zzzzzz',
+          imgUrl: ''
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    margin-top: .2rem
+    line-height: .8rem
+    background-color #eee
+    text-indent: .2rem
+  .item
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #eee
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        background-color: orange
+        margin-top: .16rem
+        padding: 0.1rem
+        border-radius: .06rem
+        color: #fff
+</style>
