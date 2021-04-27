f048a4834afbbc1e6d136d2b6b468b2fb4b040fa
shaw-555 <shaw18664590570@163.com>
2021-04-27 14:10:18 +0800

    fix:'完成周末页面'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
        <home-swiper/>
        <home-icons/>
        <home-recommend/>
+       <home-weekend/>
        <div>test</div>
     </div>
 </template>
@@ -13,6 +14,7 @@ import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 
 export default {
   name: 'Home',
@@ -20,7 +22,8 @@ export default {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item in list"
+        :key="item.id"
+        >
+        <div class="item-img-wrapper">
+          <div class="item-img" :src="item.imgUrl">zzz</div>
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}aaa</p>
+          <p class="item-desc">{{item.desc}}bbb</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  props: {
+    list: Array
+  }
+  // data () {
+  //   return {
+  //     list: [
+  //       {
+  //         id: '001',
+  //         title: 'bar',
+  //         desc: 'lfjdasdfas;djf',
+  //         imgUrl: ''
+  //       },
+  //       {
+  //         id: '002',
+  //         title: 'bar',
+  //         desc: 'lfjdasdfas;djf',
+  //         imgUrl: ''
+  //       },
+  //       {
+  //         id: '003',
+  //         title: 'bar',
+  //         desc: 'lfjdasdfas;djf',
+  //         imgUrl: ''
+  //       },
+  //       {
+  //         id: '004',
+  //         title: 'bar',
+  //         desc: 'lfjdasdfas;djf',
+  //         imgUrl: ''
+  //       }
+  //     ]
+  //   }
+  // }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    background-color: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 37.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-decs
+      line-height: .4rem
+      color: #ccc
+      ellispsis()
+</style>
