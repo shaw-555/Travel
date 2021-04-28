c89e2cb5ff0cf6cd5d5ece0f921c039c5de809fc
shaw-555 <shaw18664590570@163.com>
2021-04-28 14:01:43 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="list">
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+   display: flex
+   flex-direction: column
+   justify-content : center
+   position: absolute
+   right:0
+   top: 1.58rem
+   bottom: 0
+   width: .4rem
+   background-color #f00
+    .item
+      line-height: .4rem
+      text-align: center
+</style>
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -14,16 +14,16 @@ export default {
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
   .search
-    height .72rem
-    background $bgColor
-    padding 0 .1rem
+    height: .72rem
+    background: $bgColor
+    padding: 0 .1rem
     .search-input
-      box-sizing border-box
-      height .62rem
-      line-height .62rem
-      padding 0 .1rem
-      width 100%
-      text-align center
-      border-radius .06rem
-      color #666
+      box-sizing: border-box
+      height: .62rem
+      line-height: .62rem
+      padding: 0 .1rem
+      width: 100%
+      text-align: center
+      border-radius: .06rem
+      color: #666
 </style>
